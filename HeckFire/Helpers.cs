33f0b59e7551f93b722e9abc4dce29a49a31dc65
Tero Yrjölä <tero.yrjola@lindorff.com>
2017-07-26 10:48:56 +0300

    Create a new helper function to check if the QuestTimeArray is not valid

--- HeckFire/Helpers.cs
+++ HeckFire/Helpers.cs
@@ -19,5 +19,11 @@ namespace HeckFire
             int nextHour = Convert.ToInt32(questStartTime) + 1;
             return $"{questStartTime}:05 - {nextHour:00}:00";
         }
+
+        public static bool IsEmptyNullOrOld(this QuestTimePair[] questTimeArray)
+        {
+            return questTimeArray == null || questTimeArray.Length == 0 ||
+                   questTimeArray[0].Time != DateTime.Now.Hour.ToString("00");
+        }
     }
 }
